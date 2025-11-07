ef7ed78bafd015d84bd54b64ee625fb45beb5eca
derrickmodad <modad_db@live.bluefieldstate.edu>
2025-11-06 22:36:53 -0500

    Removing table now removes the table's file from filesystem

--- src/database.cpp
+++ src/database.cpp
@@ -98,6 +98,12 @@ bool Database::dropTable(const std::string& tableName) {
     if (it == tables.end()) {
         return false;
     }
+    try {
+        std::filesystem::remove(it->second.getTablePath());
+    } catch (const std::filesystem::filesystem_error& error) {
+        std::cout << "filesystem error: " << error.what() << std::endl;
+        return false;
+    }
     tables.erase(it);
     return true;
 }
